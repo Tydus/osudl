32a9698c35df43c1075f1a137b51ee1726a85c01
Tydus <Tydus@Tydus.org>
2012-04-23 10:26:23 +0800

    Add tornado to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='OsuDL',
-      version='1.0',
+      version='5.0',
       description='Osu! Download',
       author='Tydus Ken',
       author_email='Tydus@Tydus.org',
       url='http://Tydus.org',
-      install_requires=['mongoengine>=0.5'],
+      install_requires=[
+          'mongoengine>=0.5',
+          'tornado>=2.0',
+          ],
 #      install_requires=['Django>=1.3'],
      )
