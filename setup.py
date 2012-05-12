fc0de0fda9de7eace807825e7fb78c71561b746e
Tydus <Tydus@Tydus.org>
2012-05-12 14:56:29 +0800

    Add README, modify setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='OsuDL',
       version='5.0',
-      description='Osu! Download',
+      description='Osu! Reverse Proxy and Beatmap Downloader',
       author='Tydus Ken',
       author_email='Tydus@Tydus.org',
-      url='http://Tydus.org',
+      url='http://Tydus.org/osudl',
       install_requires=[
           'mongoengine>=0.5',
           'tornado>=2.0',
